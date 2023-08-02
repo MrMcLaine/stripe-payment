12d1a0fa6c4e8f374629aeefa56e5ab3ddf2532e
MrMcLaine <Lypovskyi@gmail.com>
2023-08-02 23:13:45 +0300

    refactor/ add in PaymentForm component children, formTitle elements as fields

--- components/paymentForm/PaymentForm.jsx
+++ components/paymentForm/PaymentForm.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from '../сheckoutForm/CheckoutForm';
-import RefundButton from '@/components/refunds/RefundButton';
 import styles from './PaymentForm.module.scss';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-const PaymentForm = () => {
-    const handleRefund = async () => {
-        console.log('Refund requested');
-    };
+const PaymentForm = ({ children, formTitle }) => {
     return (
         <div className={styles.container}>
             <Elements stripe={stripePromise}>
-                <h3>Payment Form</h3>
-                <CheckoutForm />
-                <RefundButton onClick={handleRefund} />
+                <h3>{formTitle}</h3>
+                {children}
             </Elements>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PaymentForm from '../components/PaymentForm/PaymentForm';
 import UserManager from '@/components/userManager/UserManager';
 import styles from '../styles/Main.module.scss';
+import CheckoutForm from '@/components/сheckoutForm/CheckoutForm';
+import RefundButton from '@/components/refunds/RefundButton';
 
 export default function HomePage() {
     const [user, setUser] = useState(null);
@@ -11,7 +13,11 @@ export default function HomePage() {
         <div className={styles.container}>
             <h1>Welcome to Our Payment App</h1>
             <UserManager user={user} setUser={setUser} setError={setError} />
-            <PaymentForm />
+            <PaymentForm formTitle="Payment Form">
+                <CheckoutForm />
+                <RefundButton />
+            </PaymentForm>
+
         </div>
     );
 }
